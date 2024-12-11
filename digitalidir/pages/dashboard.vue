beba8147d3b721238ab691ef225e634f584d2a31
ephrem <ephremmamo555@gmail.com>
2024-12-11 21:49:46 +0300

    add: add login page

---
+++ digitalidir/pages/dashboard.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1 class="font-bold">Dashboard</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ digitalidir/pages/login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="max-w-xl mx-auto shadow-md rounded-md p-10 mt-8 md:mt-40">
+    <h3 class="text-center text-2xl font-bold">Login</h3>
+    <form v-on:submit.prevent="handleSubmit">
+      <div class="flex flex-col gap-2 mb-4">
+        <label for="email">Email</label>
+        <input
+          class="border border-gray-300 focus:outline-none p-1 rounded-sm"
+          type="email"
+          id="email"
+        />
+      </div>
+      <div class="flex flex-col gap-2 mb-4">
+        <label for="password">Password</label>
+        <input
+          class="border border-gray-300 focus:outline-none p-1 rounded-sm"
+          type="password"
+          id="password"
+        />
+      </div>
+
+      <button
+        type="submit"
+        class="w-full bg-blue-500 p-1.5 mt-4 text-white font-semibold rounded-sm"
+      >
+        Login
+      </button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+const handleSubmit = () => {
+  window.location.href = '/dashboard';
+};
+</script>
+
+<style scoped></style>
